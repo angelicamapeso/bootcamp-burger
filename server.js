499387e5279e69b576c3c4556beccae0cc0d666d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-09 16:53:30 -0500

    Add dynamic port and listener

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+//Dynamic port
+const PORT = process.env.PORT || 3000;
+
+//Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
