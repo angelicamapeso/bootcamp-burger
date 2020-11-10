8d94f01e552975dceae1a376bd3172e974fdb0d6
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-10 13:28:31 -0500

    Add try/catch statement to connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db',
-});
+let connection;
+
+try {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db',
+  });
+} catch(err) {
+  console.error(err);
+  process.exit(1);
+}
 
 module.exports = connection.promise();
