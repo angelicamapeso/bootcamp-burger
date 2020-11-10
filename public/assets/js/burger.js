bf68c99d8729c055d01acc053fb831fd7b2bda06
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-10 13:14:29 -0500

    Add event listener to devour buttons

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,1 +1,14 @@
-console.log('Hey! I work!');
+document.querySelectorAll('.devour').forEach(button => {
+  button.addEventListener('click', function(event) {
+    const id = this.parentElement.getAttribute('data-id');
+    const newDevour = this.parentElement.getAttribute('data-newdevoured');
+
+    fetch(`api/burgers/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type' : 'application/json' },
+      body: JSON.stringify({ devoured: newDevour }),
+    }).then(response => {
+      if (response.ok) location.reload();
+    });
+  });
+});
